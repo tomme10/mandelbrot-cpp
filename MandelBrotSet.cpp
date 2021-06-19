f30f7a521e17f115160ff1b7c229619b25cf436d
Tomme10 <tcarvell7@gmail.com>
2021-06-19 16:32:52 +0100

    Changed resolution of output

--- MandelBrotSet.cpp
+++ MandelBrotSet.cpp
@@ -29,8 +29,8 @@ int mandelBrot(float real, float imag) {
 }
 
 
-const int WIDTH = 1920*15;
-const int HEIGHT = 1080*15;
+const int WIDTH = 1920;
+const int HEIGHT = 1080;
 
 const int RE_START = -2;
 const int RE_END = 1;
